c914939a33dd117a40fff71ef9435d727e1e8be7
hmoob lee <onlinegame136@gmail.com>
2021-06-14 09:10:04 +0700

    add

---
+++ bot.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Chatbot in PHP | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Chatbot trực tuyến đơn giản</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Xin chào, tôi có thể giúp gì cho bạn?</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Nội dung tin nhắn .." required>
+                <button id="send-btn">Gửi</button>
+            </div>
+        </div>
+    </div>
+
+    <script src="js.js" type="text/javascript" charset="utf-8" async defer></script>
+    
+</body>
+</html>
